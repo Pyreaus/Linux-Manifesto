ca2ebf704605eaa17de07afa4334cbf7e8a096bb
Sate Pestage <noel.ar@protonmail.com>
2022-07-16 13:35:38 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const server = { app: express(), port: 9000 };
+
+server.app.use(express.urlencoded({ extended: false }));
+server.app.use(express.json());
+server.app.use(express.static('static'));
+server.app.use(express.static('public'));
+
+server.app.put('/test',(req,res) => {res.send(`success ${req.rawHeaders} + ${req.headers['user-agent']}`)});
+server.app.get('/',(req,res) => {res.sendFile(path.join(__dirname+'/static/index.html'))});
+
+              /*  ----- APIs ------  */
+server.app.get('/api/v1/database',(req,res) => {    
+  const database = [
+    {
+      id: '~',
+      user: '~',
+      passw: '~'
+    },
+    {
+      id: '~',
+      user: '~',
+      passw: '~'
+    },
+    {
+      id: '~',
+      user: '~',
+      passw: '~'
+    }
+  ];
+  res.json(database);
+});
+
+server.app.listen(server.port,() => {console.warn(`application listening at: http://localhost:${server.port}`)}); 
